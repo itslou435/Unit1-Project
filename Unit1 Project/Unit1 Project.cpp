2332fe03a8749dba1f2d737e53495417bd6d97b0
Lou Montes <MontLou009@GoTVDSB.ca>
2021-10-13 10:30:36 -0400

    Add project files.

---
+++ Unit1 Project/Unit1 Project.cpp
@@ -0,0 +1,72 @@
+// Unit1 Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+const float preTax, postTax, totalPrice;
+float win, d, windows, doors, perimeter;
+int  length, width;
+const float tax = 0.13;
+
+void postTaxPrice(const float postTax)
+{
+	
+
+}
+void door(float doors)
+{
+	cout << (d + 12);
+	cout << (doors) << endl;
+}
+void window(float windows)
+{
+	cout << (win + 12);
+	cout << (windows) << endl;
+}
+void TotalPrice(float doors, float windows)
+{
+	cout <<  doors + windows;
+	cout << (totalPrice) << endl;
+}
+void perimeterC(float l, float w)
+{
+	perimeter = (l + w) + (l + w);
+	cout << perimeter << endl;
+}
+void FinalPrice(const float totalPrice, const float postTax)
+{
+	cout << (totalPrice * tax) ;
+	cout
+}
+
+int main()
+{	//Input
+	cout << "please enter the length of the room you would like us to trim\n";
+	cin >> length, "\n";
+	cout << "Now enter the width of the room you would like us to trim as well.\n";
+	cin >> width, "\n";
+	cout << "How many windows do you have in this room\n";
+	cin >> windows, "\n";
+	cout << "Now enter how many doors you have in this room as well\n";
+	cin >> doors, "\n";
+	system("cls");
+
+	//calculations
+	void perimeterC();
+	void doorWindow();
+	void TotalPrice();
+	void 
+	//Output
+	cout << "The perimeter of this room is: ";
+	cout << (perimeter) << endl;
+
+
+	// The Recipt
+	cout << "_________________________________________________";
+	cout << "|                                               |";
+	cout << "|                    Recipt                     |";
+
+
+}
